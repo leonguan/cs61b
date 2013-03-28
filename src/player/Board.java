cdc10f43f7eb004c5c68070a03f3c642a9a0f23e
James Jia <jamesjia@berkeley.edu>
2013-03-27 20:00:08 -0700

    Updated validMove

--- src/player/Board.java
+++ src/player/Board.java
@@ -2,7 +2,13 @@ package player;
 
 public class Board {
 	final static int BOARD_SIZE = 8;
+	final static int BLACK = 0;
+	final static int WHITE = 1;
+	final static int NONE = -1;
+
 	private int[][] array;
+	private int blackPieces;
+	private int whitePieces;
 
 	public Board() {
 		this.array = new int[BOARD_SIZE][BOARD_SIZE];
@@ -11,16 +17,21 @@ public class Board {
 				this.array[i][j] = -1;
 			}
 		}
+		this.blackPieces = 0;
+		this.whitePieces = 0;
 	}
 
-	public Board(Board b, Move m, int color ){
-		for (int i = 0; i< BOARD_SIZE; i++){
-			for (int j = 0; j< BOARD_SIZE; j++){
-				this.array[i][j]= b.array[i][j];
+	public Board(Board b, Move m, int color) {
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			for (int j = 0; j < BOARD_SIZE; j++) {
+				this.array[i][j] = b.array[i][j];
 			}
 		}
+		this.blackPieces = b.blackPieces;
+		this.whitePieces = b.whitePieces;
 		addMove(m, color);
 	}
+
 	/**
 	 * Updates the game board if the move is valid.
 	 * 
@@ -34,17 +45,23 @@ public class Board {
 		if (m.moveKind == Move.QUIT) {
 			return true;
 		}
-		if (!validMove(m)) {
+		if (!validMove(m, color)) {
 			return false;
 		}
 		if (m.moveKind == Move.ADD) {
+			if (color == Board.BLACK) {
+				this.blackPieces += 1;
+			} else {
+				this.whitePieces += 1;
+			}
 			this.array[m.x1][m.y1] = color;
+
 		} else if (m.moveKind == Move.STEP) {
 			if (this.array[m.x2][m.y2] != color) {
 				return false;
 			}
 			this.array[m.x1][m.x1] = color;
-			this.array[m.x2][m.y2] = -1;
+			this.array[m.x2][m.y2] = Board.NONE;
 		}
 		return true;
 	}
@@ -67,27 +84,33 @@ public class Board {
 	 * @return
 	 */
 	private boolean positionIsNull(Move m) {
-		return this.array[m.x1][m.y1] == -1;
+		return this.array[m.x1][m.y1] == Board.NONE;
 	}
 
 	/**
 	 * Checks if x1,y1 and x2,y2, if the moveKind is Move.STEP, are out of
-	 * bounds. Also calls positionIsNull(Move m).
+	 * bounds. Also calls positionIsNull(Move m). Checks if conditions for add/step moves are met.
 	 * 
 	 * @param m
 	 * @return
 	 */
-	private boolean validMove(Move m) {
-		if (!positionIsNull(m)){
+	private boolean validMove(Move m, int color) {
+		if (!positionIsNull(m)) {
 			return false;
 		}
 		if (m.x1 < 0 || m.x1 >= BOARD_SIZE) {
 			return false;
 		}
 		if (m.moveKind == Move.STEP) {
-			if (m.x2 < 0 || m.x2 >= BOARD_SIZE) {
+			boolean validStep = this.blackPieces == 10
+					&& this.whitePieces == 10;
+			if (m.x2 < 0 || m.x2 >= BOARD_SIZE || !validStep) {
 				return false;
 			}
+		}
+		else if (m.moveKind == Move.ADD){
+			boolean validAdd = this.blackPieces <10 && this.whitePieces <10;
+			return validAdd;
 		}
 		return true;
 	}
--- src/player/MachinePlayer.java
+++ src/player/MachinePlayer.java
@@ -16,7 +16,7 @@ public class MachinePlayer extends Player {
 	// or 1 (white). (White has the first move.)
 	public MachinePlayer(int color) {
 		this.color = color;
-		this.board= new Board();
+		this.board = new Board();
 	}
 
 	// Creates a machine player with the given color and search depth. Color is
@@ -30,16 +30,31 @@ public class MachinePlayer extends Player {
 	// Returns a new move by "this" player. Internally records the move (updates
 	// the internal game board) as a move by "this" player.
 	public Move chooseMove() {
-		//TODO update gameBoard & make move.
+		// TODO update gameBoard & make move.
+		int searched = 0;
+		while (searched < this.searchDepth) {
+			for (int i = 0; i < this.board.BOARD_SIZE; i++) {
+				for (int j = 0; j < this.board.BOARD_SIZE; j++) {
+					
+				}
+			}
+			searched++;
+		}
 		return new Move();
 	}
 
+	public Move chooseMove(int side,int alpha, int beta){
+		Move myBest = new Move();
+		Move reply;
+		
+		return new Move();
+	}
 	// If the Move m is legal, records the move as a move by the opponent
 	// (updates the internal game board) and returns true. If the move is
 	// illegal, returns false without modifying the internal state of "this"
 	// player. This method allows your opponents to inform you of their moves.
 	public boolean opponentMove(Move m) {
-		int oppColor = (this.color+1)%2;
+		int oppColor = (this.color + 1) % 2;
 		return this.board.addMove(m, oppColor);
 	}
 
@@ -49,7 +64,7 @@ public class MachinePlayer extends Player {
 	// player. This method is used to help set up "Network problems" for your
 	// player to solve.
 	public boolean forceMove(Move m) {
-		return this.board.addMove(m,this.color);
+		return this.board.addMove(m, this.color);
 	}
 
 }
