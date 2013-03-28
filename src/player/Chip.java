402cd9222cccd689f01c99df8a32a1df5ba72c15
Andrew <zeyuliu@gmail.com>
2013-03-27 19:35:56 -0700

    added Chip class to make finding networks easier

---
+++ src/player/Chip.java
@@ -0,0 +1,14 @@
+package player;
+
+public class Chip {
+	protected int x;
+	protected int y;
+	protected int color;
+	
+	public Chip(int x, int y, int color) {
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
+
+}
