d09144a46f58fd1255d3214b6e022abd44913e37
Andrew <zeyuliu@gmail.com>
2013-03-29 22:55:24 -0700

    added ChipArrayList to handle Chip objects specifically. Now, we do not have to cast into Chip objects if we retrieve items from the arraylist

--- src/ChipArrayList.java
+++ src/ChipArrayList.java
@@ -1,4 +1,46 @@
+import player.Chip;
+
 
 public class ChipArrayList extends ArrayList {
+	private Chip[] arraylist;
+	private int arraysize;
+	private int size;
+
 	
+	public ChipArrayList(int size) {
+		this.arraylist = new Chip[size];
+		this.arraysize = size;
+		this.size = 0;
+	}
+	
+	public ChipArrayList() {
+		this(10);
+	}
+	
+	public void add(Chip chip) {
+		this.add(this.size(), chip);
+	}
+	
+	public void add(int index, Chip chip) {
+		if (this.size() == this.arraysize) {
+			Chip[] newArray = new Chip[this.arraysize * 2];
+			for (int i = 0; i < this.arraysize; i++) {
+				newArray[i] = this.arraylist[i];
+				this.arraylist = newArray;
+				this.arraysize = this.arraysize * 2;
+			}
+		}
+		if (index >= this.size) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (this.arraylist[index] == null) {
+			this.size++;
+		}
+		this.arraylist[index] = chip;
+	}
+	
+	public Chip get(int index) {
+		return this.arraylist[index];
+	}
+
 }
