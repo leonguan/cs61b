450c8c898607083c8d1d4f1828aff6e9ab7d3a97
Andrew <zeyuliu@gmail.com>
2013-04-04 22:04:30 -0700

    Finished commenting the utils package

--- src/utils/IntegerArrayList.java
+++ src/utils/IntegerArrayList.java
@@ -1,7 +1,7 @@
 package utils;
 
 /**
- * Extends the ArrayList class. This subclass is used specifically to hold Chip objects.
+ * Extends the ArrayList class. This subclass is used specifically to hold Integer objects.
  * @author James Jia
  *
  */
@@ -10,15 +10,21 @@ public class IntegerArrayList {
 	private Integer[] arraylist;
 	private int size;
 
+	// Constructs an ArrayList that is expected to hold up to size Integers.
+	// Creates a size-length Integer array to hold the Integers added to the ArrayList.
 	public IntegerArrayList(int size) {
 		this.arraylist = new Integer[size];
 		this.size = 0;
 	}
 
+	// A no-arg constructor that defaults to produce a IntegerArrayList of size 10.
 	public IntegerArrayList() {
 		this(10);
 	}
 
+	
+	// "Clones" an IntegerArrayList by creating a new one of the same size and copying the objects over
+	// to the new IntegerArrayList
 	public IntegerArrayList(IntegerArrayList list) {
 		this.arraylist = new Integer[list.arraylist.length];
 		int i = 0;
@@ -30,6 +36,11 @@ public class IntegerArrayList {
 
 	}
 
+
+	// Adds m to the indicated index in the IntegerArrayList
+	// If the IntegerArrayList's array is not large enough, a new array of twice the size is constructed
+	// and references of the objects are copied from the original array to the new one.
+	// Throws an IndexOutOfBoundsException if the index is larger than the current size of the IntegerArrayList
 	public void add(Integer m) {
 		this.add(this.size(), m);
 	}
@@ -52,10 +63,12 @@ public class IntegerArrayList {
 		this.arraylist[index] = m;
 	}
 
+	// Returns the Integer at the given index.
 	public Integer get(int index) {
 		return this.arraylist[index];
 	}
 
+	// Checks whether or not the IntegerArrayList contains a given Integer object
 	public boolean contains(Integer target) {
 		for (int i = 0; i < this.size(); i++) {
 			if (target == this.get(i)) {
@@ -65,6 +78,7 @@ public class IntegerArrayList {
 		return false;
 	}
 
+	// Removes an object target from the IntegerArrayList
 	public void remove(Integer target) {
 		for (int i = 0; i < this.size(); i++) {
 			if (target == this.get(i)) {
@@ -73,10 +87,12 @@ public class IntegerArrayList {
 		}
 	}
 
+	// Returns the number of objects in the IntegerArrayList
 	public int size() {
 		return this.size;
 	}
 	
+	// A toString() method produced for testing purposes
 	public String toString(){
 		String str = "Nodes Visited: ";
 		for (int i = 0; i< this.arraylist.length;i++){
--- src/utils/MoveArrayList.java
+++ src/utils/MoveArrayList.java
@@ -1,23 +1,34 @@
 package utils;
 import player.Move;
-
+/**
+ * Extends the ArrayList class. This subclass is used specifically to hold Integer objects.
+ * @author Matthew Miller
+ */
 public class MoveArrayList extends ArrayList {
 	private Move[] arraylist;
 	private int size;
 
+	// Constructs an ArrayList that is expected to hold up to size number of Moves.
+	// Creates a size-length Move array to hold the Moves added to the ArrayList.
 	public MoveArrayList(int size) {
 		this.arraylist = new Move[size];
 		this.size = 0;
 	}
 
+	// A no-arg constructor that defaults to produce a IntegerArrayList of size 10.
 	public MoveArrayList() {
 		this(10);
 	}
 
+	// Adds a Move to the end of the MoveArrayList
 	public void add(Move m) {
 		this.add(this.size(), m);
 	}
 
+	// Adds m to the indicated index in the MoveArrayList
+	// If the MoveArrayList's array is not large enough, a new array of twice the size is constructed
+	// and references of the objects are copied from the original array to the new one.
+	// Throws an IndexOutOfBoundsException if the index is larger than the current size of the MoveArrayList
 	public void add(int index, Move m) {
 		if (this.size() == this.arraylist.length) {
 			Move[] newArray = new Move[this.arraylist.length * 2];
@@ -36,10 +47,12 @@ public class MoveArrayList extends ArrayList {
 		this.arraylist[index] = m;
 	}
 
+	// Returns the Move at the specified index of the MoveArrayList
 	public Move get(int index) {
 		return this.arraylist[index];
 	}
 
+	// Returns the size of the MoveArrayList
 	public int size() {
 		return this.size;
 	}
