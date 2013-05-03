3ae3e670df0e93c11e14ace3f46d146cc4c8dc03
Jihui Jin <thejihuijin@gmail.com>
2013-05-03 12:03:44 -0700

    fixed minor bugs in kruskal

--- src/Kruskal.java
+++ src/Kruskal.java
@@ -117,18 +117,19 @@ public class Kruskal {
 	public static void quickSort(LinkedQueue q) {
 		// Your solution here.
 	    if (!q.isEmpty()) {
-                Comparable pivot = (Comparable) ((KruskalEdge) q.nth(i)).getWeight()
-                LinkedQueue qSmall = new LinkedQueue();
-                LinkedQueue qLarge = new LinkedQueue();
-                LinkedQueue qEquals = new LinkedQueue();
-                partition(q, pivot, qSmall, qEquals, qLarge);
-                quickSort(qSmall);
-                quickSort(qLarge);
-                q.append(qSmall);
-                q.append(qEquals);
-                q.append(qLarge);
-            }
-	}
+        int i = (int) (q.size() * Math.random());
+        Comparable pivot = (Comparable) ((KruskalEdge) q.nth(i)).getWeight();
+        LinkedQueue qSmall = new LinkedQueue();
+        LinkedQueue qLarge = new LinkedQueue();
+        LinkedQueue qEquals = new LinkedQueue();
+        partition(q, pivot, qSmall, qEquals, qLarge);
+        quickSort(qSmall);
+        quickSort(qLarge);
+        q.append(qSmall);
+        q.append(qEquals);
+        q.append(qLarge);
+    }
+}
 
 
 
